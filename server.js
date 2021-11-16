e04c6cd8659218f85a38dd7be8b124b1c02e8be6
shidqi <nauvalholic@gmail.com>
2021-11-16 22:57:06 +0700

    fix nodejs cd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const { greeting } = require('./user');
 
-app.get('/', (req, res) => res.send('This App is running properly!'));
+app.get('/', (req, res) => res.send('This App is running properly NAUVAL2!'));
 app.get('/ping', (req, res) => res.send('Pong!'));
 app.get('/youtube', (req, res) => res.send('Hello, youtube indonesia2!'));
 app.get('/hello/:name', (req, res) => {
