09afe1d229e4b07f5cccf0938be194598e2e5ef7
Shidqi Nauval <41341446+nauvalsh@users.noreply.github.com>
2021-11-17 13:08:25 +0700

    update test 444

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const { greeting } = require('./user');
 
-app.get('/', (req, res) => res.send('This App is running properly NAUVAL3!'));
+app.get('/', (req, res) => res.send('This App is running properly NAUVAL444444!'));
 app.get('/ping', (req, res) => res.send('Pong!'));
 app.get('/youtube', (req, res) => res.send('Hello, youtube indonesia2!'));
 app.get('/hello/:name', (req, res) => {
